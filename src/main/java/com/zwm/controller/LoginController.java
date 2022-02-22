2defa894b96e8c7084f3d16b575b463cb47fc2ee
lucky <krolliaa@163.com>
2022-02-22 23:09:57 +0800

    完成注册页面显示

---
+++ src/main/java/com/zwm/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.zwm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/register";
+    }
+}
