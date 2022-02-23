6d8a890f9b6a1530f110e2b265aa3278dfe73fcd
lucky <krolliaa@163.com>
2022-02-24 00:21:26 +0800

    会话管理之 cookie 的学习

--- src/main/java/com/zwm/controller/DiscussPostController.java
+++ src/main/java/com/zwm/controller/DiscussPostController.java
@@ -1,11 +1,13 @@
 package com.zwm.controller;
 
 import com.zwm.service.impl.DiscussPostServiceImpl;
+import com.zwm.util.CommunityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/discussPost", method = RequestMethod.GET)
@@ -22,5 +24,21 @@ public class DiscussPostController {
     @RequestMapping(value = "/select2")
     public Object findDiscussPostsCount() {
         return discussPostService.findDiscussPostsCount(1);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/getCookie", method = RequestMethod.GET)
+    public String sendCookie(HttpServletResponse httpServletResponse) {
+        Cookie cookie = new Cookie("code", CommunityUtils.generateUUID());
+        cookie.setPath("/community/discussPost");
+        cookie.setMaxAge(60 * 10);
+        httpServletResponse.addCookie(cookie);
+        return "add Cookie!";
+    }
+
+    @RequestMapping(value = "/sendCookieToServer", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue(value = "code") String code) {
+        return "get Cookie: code ---> " + code;
     }
 }
--- src/main/java/com/zwm/util/CommunityUtils.java
+++ src/main/java/com/zwm/util/CommunityUtils.java
@@ -6,12 +6,12 @@ import org.springframework.util.DigestUtils;
 import java.util.UUID;
 
 public class CommunityUtils {
-    //获取随机字符串用于生成激活码、密码加盐等
+    //生成随机数
     public static String generateUUID() {
-        return UUID.randomUUID().toString().replaceAll(",", "");
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
-    //密码加盐加MD5
+    //MD5加密
     public static String md5(String key) {
         if (StringUtils.isBlank(key)) return null;
         return DigestUtils.md5DigestAsHex(key.getBytes());
