30e07095609ad214cd941d46a0cd6661969ab9c6
krolliaa <krolliaa@163.com>
2022-02-24 10:30:19 +0800

    完成 Session 会话管理学习

--- src/main/java/com/zwm/controller/DiscussPostController.java
+++ src/main/java/com/zwm/controller/DiscussPostController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping(value = "/discussPost", method = RequestMethod.GET)
@@ -40,5 +40,20 @@ public class DiscussPostController {
     @ResponseBody
     public String getCookie(@CookieValue(value = "code") String code) {
         return "get Cookie: code ---> " + code;
+    }
+
+    @RequestMapping(value = "/setSession", method = RequestMethod.GET)
+    @ResponseBody
+    public String testSession(HttpSession httpSession) {
+        //Session 比 Cookie 强大，它可以设置任何类型的数据
+        httpSession.setAttribute("id", 888);
+        httpSession.setAttribute("name", "wow");
+        return "Set Session Ok";
+    }
+
+    @RequestMapping(value = "/getSession", method = RequestMethod.GET)
+    @ResponseBody
+    public String testSession(@CookieValue(value = "JSESSIONID") String JSESSIONID) {
+        return "Get Session OK: " + JSESSIONID;
     }
 }
