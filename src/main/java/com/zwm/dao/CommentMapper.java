48cb2f0c2c552958b3647fb9a77dc6f43ca8843d
krolliaa <krolliaa@163.com>
2022-03-02 14:25:26 +0800

    完成 Comment 评论数据层Dao层 1. CommentMapper.java 2. CommentMapper.xml

---
+++ src/main/java/com/zwm/dao/CommentMapper.java
@@ -0,0 +1,17 @@
+package com.zwm.dao;
+
+import com.zwm.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    //entityType 决定评论类型：1-帖子评论 2-回复评论
+    //entityId 决定发表评论的实体 Id
+    public abstract List<Comment> selectCommentsByEntity(int entityType, int entityId, int start, int end);
+
+    //entityType：1-帖子评论 2-回复评论
+    //entityId：发表评论的实体 Id
+    public abstract int selectCountByEntity(int entityType, int entityId);
+}
