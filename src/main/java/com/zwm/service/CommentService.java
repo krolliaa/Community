4fa134a37cd3bab213ad63508bc0488543522045
krolliaa <krolliaa@163.com>
2022-03-02 14:31:12 +0800

    完成 Comment 评论业务层Service层 1. CommentService.java 2. CommentServiceImpl.java

---
+++ src/main/java/com/zwm/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.zwm.service;
+
+import com.zwm.entity.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+    //分页查找当前所有评论
+    public abstract List<Comment> findCommentsByEntity(int entityType, int entityId, int start, int end);
+
+    //根据实体查找评论数量
+    public abstract int findCountByEntity(int entityType, int entityId);
+}
---
+++ src/main/java/com/zwm/service/impl/CommentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.zwm.service.impl;
+
+import com.zwm.dao.CommentMapper;
+import com.zwm.entity.Comment;
+import com.zwm.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CommentServiceImpl implements CommentService {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Override
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int start, int end) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, start, end);
+    }
+
+    @Override
+    public int findCountByEntity(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+}
