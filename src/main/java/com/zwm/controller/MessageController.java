eb7e509f3f3af8e0bb169ad2b7a01133196b762e
krolliaa <krolliaa@163.com>
2022-03-04 09:44:52 +0800

    完成私信详情

--- src/main/java/com/zwm/controller/MessageController.java
+++ src/main/java/com/zwm/controller/MessageController.java
@@ -9,6 +9,7 @@ import com.zwm.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -62,5 +63,43 @@ public class MessageController {
         int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
         model.addAttribute("letterUnreadCount", letterUnreadCount);
         return "/site/letter";
+    }
+
+    @RequestMapping(value = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getConversationDetail(@PathVariable(value = "conversationId") String conversationId, Model model, Page page) {
+        //分页信息
+        page.setLimit(5);
+        page.setPath("/letter/detail/" + conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+        //私信列表
+        List<Message> letterList = messageService.findLetters(conversationId, page.getStart(), page.getLimit());
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        //每条私信里面有用户头像信息
+        if (letterList != null) {
+            for (Message letter : letterList) {
+                //使用 Map 数据结构存储数据
+                Map<String, Object> map = new HashMap<>();
+                map.put("letter", letter);
+                map.put("fromUser", userService.findUserById(letter.getFromId()));
+                //将 Map 数据结构资源包放到一个大的资源包中
+                mapList.add(map);
+            }
+        }
+        model.addAttribute("letters", mapList);
+        model.addAttribute("target", getLetterTarget(conversationId));
+        return "/site/letter-detail";
+    }
+
+    //不要显示当前用户的信息，尽量显示他人的用户信息
+    private User getLetterTarget(String conversationId) {
+        String[] ids = conversationId.split("_");
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+
+        if (hostHolder.getUser().getId() == id0) {
+            return userService.findUserById(id1);
+        } else {
+            return userService.findUserById(id0);
+        }
     }
 }
