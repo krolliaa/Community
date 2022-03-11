35fceb59ed3806daa1897ddeac73b82dc7cfe423
krolliaa <krolliaa@163.com>
2022-03-12 06:01:17 +0800

    尚未完成生产者、消费者

--- src/main/java/com/zwm/event/EventConsumer.java
+++ src/main/java/com/zwm/event/EventConsumer.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import java.util.IdentityHashMap;
 
 import java.util.Date;
 import java.util.HashMap;
