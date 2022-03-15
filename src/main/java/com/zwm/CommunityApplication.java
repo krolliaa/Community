7767ac7628a5b2f20463b276dddbbaed44aba41c
krolliaa <krolliaa@163.com>
2022-03-15 16:16:43 +0800

    解决ElasticSearch和Redis底层Netty冲突

--- src/main/java/com/zwm/CommunityApplication.java
+++ src/main/java/com/zwm/CommunityApplication.java
@@ -3,8 +3,17 @@ package com.zwm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+
+    @PostConstruct
+    public void init() {
+        //解决ElasticSearch和Redis底层Netty冲突
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
     }
