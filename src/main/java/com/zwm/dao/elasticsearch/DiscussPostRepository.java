e34674944531cd292f6363c31ab38138fa230467
krolliaa <krolliaa@163.com>
2022-03-15 18:47:42 +0800

    1. 创建帖子索引等信息 ---> 注解 2. 创建 DiscussPost 帖子搜索仓库

---
+++ src/main/java/com/zwm/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,9 @@
+package com.zwm.dao.elasticsearch;
+
+import com.zwm.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+}
--- src/main/java/com/zwm/entity/DiscussPost.java
+++ src/main/java/com/zwm/entity/DiscussPost.java
@@ -3,21 +3,35 @@ package com.zwm.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(indexName = "discusspost", indexStoreType = "_doc", shards = 6, replicas = 3)
 public class DiscussPost {
+    @Id
     private int id;//帖子ID
+    @Field(type = FieldType.Integer)
     private int userId;//用户ID
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title;//帖子标题
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;//帖子内容
+    @Field(type = FieldType.Integer)
     private int type;//帖子类型：0-普通 + 1-置顶
+    @Field(type = FieldType.Integer)
     private int status;//帖子状态：0-普通 + 1-精华 + 2-拉黑
+    @Field(type = FieldType.Integer)
     private int commentCount;//帖子评论数量
+    @Field(type = FieldType.Date)
     private Date createTime;//帖子创建日期
+    @Field(type = FieldType.Double)
     private Double score;//帖子获得的分数
 
     @Override
