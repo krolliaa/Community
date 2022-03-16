5f8e9174dbdfc82d54fd7c6f2e10ea8973bcd518
krolliaa <krolliaa@163.com>
2022-03-16 17:27:20 +0800

    完成搜索功能

--- src/main/java/com/zwm/event/EventConsumer.java
+++ src/main/java/com/zwm/event/EventConsumer.java
@@ -2,8 +2,12 @@ package com.zwm.event;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.zwm.dao.DiscussPostMapper;
+import com.zwm.entity.DiscussPost;
 import com.zwm.entity.Event;
 import com.zwm.entity.Message;
+import com.zwm.service.impl.DiscussPostServiceImpl;
+import com.zwm.service.impl.ElasticsearchServiceImpl;
 import com.zwm.service.impl.MessageServiceImpl;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
@@ -24,24 +28,19 @@ public class EventConsumer {
     @Autowired
     private MessageServiceImpl messageService;
 
+    @Autowired
+    private DiscussPostServiceImpl discussPostService;
+
+    @Autowired
+    private ElasticsearchServiceImpl elasticsearchService;
+
     private static Logger logger = LoggerFactory.getLogger(EventConsumer.class);
 
     //消费者消费消息 ---> 生产者生产的是事件对象
     @KafkaListener(groupId = "test-consumer-group", topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void handlerEvent(ConsumerRecord consumerRecord) {
-        //如果消费到的消息是null或者是空的，日志报错
-        if (consumerRecord == null || consumerRecord.value() == null) {
-            logger.error("消息的内容为空!");
-            return;
-        }
-
-        //获取到 JSON 对象转化为 Event 事件对象
-        Event event = JSON.parseObject(consumerRecord.value().toString(), Event.class);
-        if (event == null) {
-            logger.error("消息格式错误!");
-            return;
-        }
-
+        Event event = handleException(consumerRecord);
+        if (event == null) return;
         //获取到消息就可以发送系统消息了，当时发送消息的时候使用的是 MessageService 所以这里要使用 MessageService
         //可以打印一下获取到的消息
         System.out.println(event.toString());
@@ -68,5 +67,32 @@ public class EventConsumer {
         message.setContent(JSONObject.toJSONString(content));
         //将消息存入到数据库中
         messageService.insertMessage(message);
+    }
+
+    //消费发帖消息
+    @KafkaListener(groupId = "test-consumer-group", topics = {TOPIC_PUBLISH})
+    public void consumePublish(ConsumerRecord consumerRecord) {
+        Event event = handleException(consumerRecord);
+        if (event == null) return;
+        //获取到帖子Id ---> event.getEntityId
+        DiscussPost discussPost = discussPostService.selectDiscussPost(event.getEntityId());
+        elasticsearchService.insertPostToES(discussPost);
+    }
+
+    //处理消费异常
+    public Event handleException(ConsumerRecord consumerRecord) {
+        //如果消费到的消息是null或者是空的，日志报错
+        if (consumerRecord == null || consumerRecord.value() == null) {
+            logger.error("消息的内容为空!");
+            return null;
+        }
+
+        //获取到 JSON 对象转化为 Event 事件对象
+        Event event = JSON.parseObject(consumerRecord.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return null;
+        }
+        return event;
     }
 }
